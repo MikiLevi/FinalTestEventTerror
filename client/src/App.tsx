02c042119f735bad05fa25e13343a022d6b49089
Miki_Levi <miclevi101@gmail.com>
2024-12-24 20:04:51 +0200

    Clean aal garbage for build

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,7 @@
 import "./App.css";
 import ElevateAppBar from "./components/app/AppBr";
-// import DisplayAttackes from "./components/attack/DisplayAttackes";
-// import DisplayOrganizations from "./components/organization/DisplayOrganizations";
-import MapComponent from "./components/regions/MapRegions";
 import Router from "./router/router";
-// import DisplayTime from "./components/time/DisplayIncidents";
+
 
 function App() {
   return (
