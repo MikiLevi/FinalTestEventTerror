fb30508a1f8459e01e5b6b6b48d57d6378e51deb
Miki_Levi <miclevi101@gmail.com>
2024-12-24 16:42:33 +0200

    Add Length and width

--- server/src/controllers/dataContoller.ts
+++ server/src/controllers/dataContoller.ts
@@ -8,6 +8,7 @@ import {
   getTerrorOrgByYear,
 } from "../services/dataService";
 import { handleError } from "../../utils/ErrorHandle";
+import { ITerror } from "../models/terror";
 
 const router: IRouter = express.Router();
 
@@ -25,24 +26,38 @@ const DeadliestTerrorism = async (
 
 const HighCasualtyArea = async (req: Request, res: Response): Promise<void> => {
   try {
-    const terrorism = await getHighCasualtyArea();
+    const terrorism:ITerror[] = await getHighCasualtyArea();
     res.json(terrorism);
   } catch (error: any) {
     handleError(res, error.status || 404, error.message);
   }
 };
 
+// export const getTerrorEventsByCasualtiesInRegions = async (
+//   req: Request,
+//   res: Response
+// ) => {
+//   try {
+//     const TerrorEvents: ITerrorEvent[] | null = await RegionsByCasualties();
+//     if (!TerrorEvents) {
+//       res.status(404).json({ msg: "Terror Events not found" });
+//       return;
+//     }
+//     res.json(TerrorEvents);
+//   } catch (error) {
+//     res.status(500).json({ msg: "Server error" + error });
+//   }
+// };
+
 const IncidentByDate = async (req: Request, res: Response): Promise<void> => {
   try {
-    //const test = req.query
-    
     const yearStart = Number(req.query.yearStart);
     const yearEnd = Number(req.query.yearEnd);
     const monthStart = Number(req.query.monthStart);
     const monthEnd = Number(req.query.monthEnd);
-    const dateToSearch = { yearStart, yearEnd, monthStart, monthEnd }
+    const dateToSearch = { yearStart, yearEnd, monthStart, monthEnd };
     console.log(yearStart, yearEnd);
-    
+
     const terrorism = await getIncidentByDate(dateToSearch);
     res.json(terrorism);
   } catch (error: any) {
@@ -55,7 +70,7 @@ const TerrorOrgByRegions = async (req: Request, res: Response) => {
     const regionName = req.query.regionName as string;
     const limit = Number(req.query.limit);
     console.log(regionName, limit);
-    
+
     const TerrorEvents = await getTerrorOrgByRegions(regionName, limit);
     if (!TerrorEvents) {
       res.status(404).json({ msg: "Terror Events not found" });
