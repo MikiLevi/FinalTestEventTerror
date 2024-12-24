74eb2e6231f7c87adab9e50bfedf010ad24918fc
Miki_Levi <miclevi101@gmail.com>
2024-12-24 17:40:00 +0200

    Add  all

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
 import "./App.css";
-import { MyMap } from "./components/DisplayRegions";
+import DisplayAttackes from "./components/attack/DisplayAttackes";
+import DisplayOrganizations from "./components/organization/DisplayOrganizations";
+import MapComponent from "./components/regions/MapRegions";
+import DisplayTime from "./components/time/DisplayIncidents";
+// import { DisplayTime } from "./components/time/DisplayIncidents";
 
 function App() {
   return (
     <>
       {/* <DisplayAttackes /> */}
-      <MyMap />
+      <MapComponent />
+      {/* <DisplayTime/> */}
+      {/* <DisplayOrganizations /> */}
     </>
   );
 }
