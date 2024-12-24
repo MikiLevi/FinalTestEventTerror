a0dc235b112a54837586b54db4a7d9e0ac50d4ef
Miki_Levi <miclevi101@gmail.com>
2024-12-24 15:33:32 +0200

    Add api route

--- server/router/router.ts
+++ server/router/router.ts
@@ -14,9 +14,9 @@ const router: IRouter = express.Router();
 router.get("/api/analysis/deadliest-attack-types/", DeadliestTerrorism);
 router.get("/api/analysis/highest-casualty-regions/", HighCasualtyArea);
 router.get("/api/analysis/incident-trends/", IncidentByDate);
-router.get("/relationships/top-groups", TerrorOrgByRegions);
-router.get("/relationships/groups-by-year", TerrorOrgByYear);
-router.get("/relationships/deadliest-regions", DeadliestRegionsByGroup);
+router.get("/api/relationships/top-groups", TerrorOrgByRegions);
+router.get("/api/relationships/groups-by-year", TerrorOrgByYear);
+router.get("/api/relationships/deadliest-regions", DeadliestRegionsByGroup);
 
 router.use((req: Request, res: Response) => {
   handleError(res, 404, "page is not found");
